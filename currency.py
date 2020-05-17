d8a5df0b9429c9c472792872a68b5a09214ad551
Bhargavi-bj <64141036+Bhargavi-bj@users.noreply.github.com>
2020-05-17 16:14:17 +0530

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://currencyapi.net/api/v1/rates?key=8S6WV4dEvTHzScNFZSscke0FRO7f8fEJbsFh"
+
+
+
+response = requests.get(url)
+recieved_data= response.json()
+for data in recieved_data.keys():
+    if data=='rates':
+        currency_rate=recieved_data[data]
+        for ratesofMoney in currency_rate.keys():
+            if ratesofMoney=='INR':
+                print("Rate of Indian rupees against 1 dollar : {}".format(currency_rate[ratesofMoney]))
+
+#print(response.text.encode('utf8'))
